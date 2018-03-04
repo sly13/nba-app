754100d0a4005c29b786a37baadbd195b14f8bb8
vadim semenko <vadimsemenko@gmail.com>
2018-03-04 15:12:47 +0300

    enject + add navSide

--- src/components/Header/SideNav/SideNav.js
+++ src/components/Header/SideNav/SideNav.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
+import SideNav from "react-simple-sidenav";
+import SideNavItems from "./sideNav_items";
 
-class SideNavigator extends Component {
-  render() {
-    return <div>SideNav</div>;
-  }
-}
+const SideNavigator = props => {
+  return (
+    <div>
+      <SideNav
+        showNav={props.showNav}
+        onHideNav={props.onHideNav}
+        navStyle={{
+          background: "#242424",
+          maxWidth: "220px"
+        }}
+      >
+        <SideNavItems />
+      </SideNav>
+    </div>
+  );
+};
 
 export default SideNavigator;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,31 +1,33 @@
 import React from "react";
-import SideNavigator from "./SideNav/SideNav";
+import SideNav from "./SideNav/sideNav";
 import style from "./header.css";
 import FontAwersome from "react-fontawesome";
 import { Link } from "react-router-dom";
 
 const Header = props => {
-  const navBars = () => {
+  const navBars = () => (
     <div className={style.bars}>
       <FontAwersome
+        name="bars"
+        onClick={props.onOpenNav}
         style={{
           color: "#dfdfdf",
           padding: "10px",
           cursor: "pointer"
         }}
       />
-    </div>;
-  };
+    </div>
+  );
 
-  const logo = () => {
+  const logo = () => (
     <Link to="/" className={style.logo}>
       <img alt="nba logo" src="/images/nba_logo.png" />
-    </Link>;
-  };
+    </Link>
+  );
 
   return (
     <header className={style.header}>
-      <SideNavigator />
+      <SideNav {...props} />
       <div className={style.headerOpt}>
         {navBars()}
         {logo()}
